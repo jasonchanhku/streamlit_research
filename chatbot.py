1798da779287a28f2b1f40e95334c93ebc14dcbd
Jason Chan <jasonchanhku@gmail.com>
2023-08-04 01:36:06 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -30,7 +30,7 @@ def format_data():
     variable.markdown("", unsafe_allow_html=True)
 
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
-    """Return the number of tokens used by a list of messages."""
+    """Return the number of tokens used by a list of messages in out."""
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
